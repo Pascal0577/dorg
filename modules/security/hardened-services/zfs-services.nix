d8510742be7756d066288463657b8ed0e7829a45
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-07-08 17:03:24 -0400

    init: modules/security/hardened-services/zfs-services.nix

---
+++ modules/security/hardened-services/zfs-services.nix
@@ -0,0 +1,57 @@
+{ config, lib, ... }:
+let zfsMaintenance = {
+    NoNewPrivileges = true;
+    ProtectHostname = true;
+    ProtectClock = true;
+    ProtectKernelLogs = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    PrivateTmp = true;
+    PrivateNetwork = true;
+    RestrictSUIDSGID = true;
+    RestrictRealtime = true;
+    LockPersonality = true;
+    SystemCallArchitectures = "native";
+    SystemCallFilter = [
+        "~@cpu-emulation"
+        "~@debug"
+        "~@obsolete"
+        "~@reboot"
+        "~@swap"
+        "~@clock"
+        "~@module"
+    ];
+    RestrictAddressFamilies = [ "AF_UNIX" ];
+    UMask = "0077";
+};
+in
+{
+    config = lib.mkIf config.mySystem.ZFS.enable {
+        systemd.services.zfs-zed.serviceConfig = {
+            NoNewPrivileges = true;
+            ProtectHostname = true;
+            ProtectClock = true;
+            ProtectKernelLogs = true;
+            ProtectHome = true;
+            PrivateTmp = true;
+            RestrictSUIDSGID = true;
+            RestrictRealtime = true;
+            LockPersonality = true;
+            SystemCallArchitectures = "native";
+            SystemCallFilter = [
+                "~@cpu-emulation"
+                "~@debug"
+                "~@obsolete"
+                "~@reboot"
+                "~@swap"
+                "~@clock"
+            ];
+            RestrictAddressFamilies = [ "AF_UNIX" "AF_NETLINK" ];
+            UMask = "0077";
+        };
+
+        systemd.services.zfs-scrub.serviceConfig = zfsMaintenance;
+        systemd.services.zpool-trim.serviceConfig = zfsMaintenance;
+    };
+}
+
