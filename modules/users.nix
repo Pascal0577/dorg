041766a16f50917fd96a4fe638fb2fec6bd319df
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-07-08 16:39:30 -0400

    init: modules/users.nix

---
+++ modules/users.nix
@@ -0,0 +1,7 @@
+{
+    users.users.dorg = {
+        initialHashedPassword = "$y$j9T$YzObaOLT0bL9jg80m89MF0$zBiJafeF83tn7gJDB6dWqCluDeXcOcnh.mC04N6Rdf7";
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+    };
+}
