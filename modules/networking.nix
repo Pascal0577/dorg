5b25fb86f701a3cd37d1d5758139f92e76d31524
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-07-08 16:08:24 -0400

    init: modules/networking.nix

---
+++ modules/networking.nix
@@ -0,0 +1,36 @@
+{
+    systemd.network.enable = true;
+
+    networking = {
+        hostName = "pi";
+        useNetworkd = true;
+        firewall = {
+            enable = true;
+            allowedTCPPorts = [ 22 ];
+        };
+    };
+    
+    services.resolved = {
+        enable = true;
+        settings.Resolve = {
+            DNS = "9.9.9.9#dns.quad9.net";
+            FallbackDNS = "1.1.1.1#cloudflare-dns.com 8.8.8.8#dns.google";
+            DNSSEC = "allow-downgrade";
+            DNSOverTLS = true;
+        };
+    };
+
+    systemd.network.networks."10-wired" = {
+        matchConfig.Name = "enp1s0";
+        networkConfig = {
+            DHCP = "yes";
+            IPv6AcceptRA = true;
+        };
+        ipv6AcceptRAConfig = {
+            UseAutonomousPrefix = true;
+            Token = "prefixstable";
+        };
+        dhcpV4Config.RouteMetric = 10;
+        linkConfig.RequiredForOnline = "routable";
+    };
+}
