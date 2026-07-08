9694360d14a8f5006090808ef5ce036699ce8f08
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-07-08 17:44:26 -0400

    systems/armaku: remove an unneeded kernel module

--- systems/armaku/hardware-configuration.nix
+++ systems/armaku/hardware-configuration.nix
@@ -3,11 +3,8 @@
     hardware.cpu.amd.updateMicrocode = true;
     hardware.enableRedistributableFirmware = true;
 
-    boot = {
-        kernelModules = [ "kvm-amd" ];
-        initrd = {
-            availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-            kernelModules = [ "dm-snapshot" ];
-        };
+    boot.initrd = {
+        availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+        kernelModules = [ "dm-snapshot" ];
     };
 }
