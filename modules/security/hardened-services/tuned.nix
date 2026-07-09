b360feb34d4d7b98417d9674598f32e52e5452b2
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-07-09 12:10:07 -0400

    hardened-services/tuned.nix: ensure state directories exist

--- modules/security/hardened-services/tuned.nix
+++ modules/security/hardened-services/tuned.nix
@@ -1,4 +1,4 @@
-{ hardening, ... }:
+{ hardening, pkgs, ... }:
 let
     default = hardening.defaultProfile;
 in
@@ -26,6 +26,19 @@ in
 
             RestrictAddressFamilies = default.RestrictAddressFamilies ++ [ "AF_NETLINK" ];
             ReadWritePaths = [ /var/log/tuned /etc/tuned ];
+        };
+    };
+
+    # systemd needs /var/log/tuned and /etc/tuned to exist to set up namespaces
+    systemd.services.make-tuned-dirs = {
+        enable = true;
+        before = [ "tuned-ppd.service" "tuned.service" ];
+        wantedBy = [ "tuned-ppd.service" "tuned.service" ];
+        description = "Make `tuned` state directories so setting up namespaces doesn't fail";
+        serviceConfig = hardening.mkService {
+            Type = "oneshot";
+            ProtectSystem = false;
+            ExecStart = "${pkgs.coreutils-full}/bin/mkdir -p /var/log/tuned /etc/tuned";
         };
     };
 }
